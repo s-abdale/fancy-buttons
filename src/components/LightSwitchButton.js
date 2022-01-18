76366b253721f539949af2871aa4c76b99315854
Safia Abdale <safia.abdale@gmail.com>
2022-01-18 14:13:30 -0500

    Added conditional rendering to lightSwitchButton being on or off

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 
 function LightSwitchButton(props){
   const [light, setLight] = useState();
-  
+
   return (
     <button className="LightSwitchButton">
-      {/* When the state is on */}<span className="on"><i>💡</i> I'm on!</span>
-      {/* When the state is off */}<span className="off"><i>💡</i> I'm off!</span>
+      {/* When the state is on */}
+      {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
+
+      {/* When the state is off */}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 }
