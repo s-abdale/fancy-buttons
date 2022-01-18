25a45ef4271a2163470633c39c0b988294b18a74
Safia Abdale <safia.abdale@gmail.com>
2022-01-18 14:11:14 -0500

    Added style, added conditional rendering for anger<1 and anger>1

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -4,9 +4,10 @@ function AngryButton(props){
   const [anger, setAnger] = useState();
 
   return (
-    <button className="AngryButton">
-      {/* If you have NOT reached the maximum */}<span>Don't click me too much! </span>
-      {/* If you HAVE reached the maximum */}<span>Rawr!</span>
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} className="AngryButton">
+      {/* If you have NOT reached the maximum */}{anger < 1 && <span>Don't click me too much! </span>}
+
+      {/* If you HAVE reached the maximum */}{anger > 1 && <span>Rawr!</span>}
     </button>
   );
 }
